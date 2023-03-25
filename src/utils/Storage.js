2b9ad67bf9b18093f86e4ac38702f9dc4897c1c4
Zane Helton <zane99@me.com>
2023-03-24 23:58:24 -0400

    Add currency conversion for donations

--- src/utils/Storage.js
+++ src/utils/Storage.js
@@ -10,33 +10,53 @@ const storeDonation = async ({
   amount,
   currency,
 }) => {
-  const resp = await fetch(
-    `${process.env.STREAMAZE_STORAGE_API_URL}/api/donations`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        streamer_id: streamerId,
-        type,
-        sender,
-        amount_in_usd, // TODO: Convert amount to USD server-side
-        message,
-        metadata,
-        value: {
-          amount,
-          currency,
-        },
-      }),
-    },
-  )
+  try {
+    let amountInUSD = amount_in_usd
+    if (currency.toLowerCase() !== 'usd') {
+      const currencyRateToUSD = await fetch(
+        `${process.env.EXCHANGE_RATE_API}/v1/rates/${currency.toLowerCase()}`,
+      )
 
-  if (resp.ok) {
-    console.log('Created a donation')
-  } else {
-    console.log(await resp.text())
-    console.log('Failed to create a donation')
+      if (currencyRateToUSD.ok) {
+        const jsonResp = await currencyRateToUSD.json()
+        const usdConversionRate = parseFloat(
+          jsonResp?.data?.[currency.toLowerCase()],
+        )
+
+        amountInUSD = (amount / 100 / usdConversionRate).toFixed(2)
+      }
+    }
+
+    const resp = await fetch(
+      `${process.env.STREAMAZE_STORAGE_API_URL}/api/donations`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          streamer_id: streamerId,
+          type,
+          sender,
+          amount_in_usd: amountInUSD,
+          message,
+          metadata,
+          value: {
+            amount,
+            currency,
+          },
+        }),
+      },
+    )
+
+    if (resp.ok) {
+      console.log('Created a donation')
+    } else {
+      console.log(await resp.text())
+      console.log('Failed to create a donation')
+    }
+  } catch (err) {
+    console.error(err)
   }
 }
 
